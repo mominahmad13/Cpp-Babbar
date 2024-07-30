53533125fae3997945111a91c6e07869ab58a9aa
mominahmadrao <164503533+mominahmadrao@users.noreply.github.com>
2024-07-30 15:33:54 +0500

    Add tweleveth  pattern file to local repo

---
+++ Babbar/Loops/While/Pattern12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: \n";
+    cin >> n;
+    int row = 1;
+     while (row <= n)
+     {
+        int col = 1;
+        while ( col <= n)
+        {
+           cout<< static_cast <char> ('A'+ col -1) << " ";
+           col++;
+        }
+        cout<< endl;
+        row++;
+     }
+     
+
+    return 0;
+}
