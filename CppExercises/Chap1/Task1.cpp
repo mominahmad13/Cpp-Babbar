8564f6477fcdfc39e68c0bd0d2aef7ff2ad7c170
mominahmadrao <164503533+mominahmadrao@users.noreply.github.com>
2024-07-30 16:29:16 +0500

    Add book's first task to local repo

---
+++ CppExercises/Chap1/Task1.cpp
@@ -0,0 +1,9 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Introduction to C++" << endl;
+    cout<< "Welcome to Object Oriented Programming" << endl;
+    return 0;
+}
