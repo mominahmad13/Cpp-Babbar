55fc5695caa26411f06ccb9b886f84cea1e0b161
mominahmadrao <164503533+mominahmadrao@users.noreply.github.com>
2024-07-30 15:28:37 +0500

    Add ninth(a) pattern file to local repo

---
+++ Babbar/Loops/While/Pattern9a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: \n";
+    cin >> n;
+    int row = 1 ; 
+    
+    while (row <= n)
+    {
+      int col = 1;
+      int value = row ;
+      while (col <= row )
+      {
+        cout<< value << " ";
+        col ++ ;
+        value++;
+      } 
+       cout << endl;
+       row++;
+
+    }
+    
+    return 0;
+}
