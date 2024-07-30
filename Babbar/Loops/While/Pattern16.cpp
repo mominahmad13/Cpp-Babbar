c565df150effc4aade3e766abc39eb62bd08f78e
mominahmadrao <164503533+mominahmadrao@users.noreply.github.com>
2024-07-30 15:38:21 +0500

    Add sixteenth  pattern file to local repo

---
+++ Babbar/Loops/While/Pattern16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cout<<"Enter n : \n ";
+    cin >> n;
+    int row = 1 ;
+    char ch = 'A';
+
+    while ( row <= n)
+    {
+        int col = 1 ;
+        while ( col <= row )
+        {   
+            cout << ch++ << " ";
+            col++ ;
+        }
+        cout<< endl;
+        row++;
+
+    }
+    
+    return 0;
+}
