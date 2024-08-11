27f589ff990c0d242dbab6ab5ee3deb69a31336a
mominahmadrao <164503533+mominahmadrao@users.noreply.github.com>
2024-08-11 14:47:54 +0500

    Add intro program for learning switch

---
+++ Babbar/Switch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std ;
+
+int main ()
+{
+    char ch  = '1' ;
+    int num = 1 ;
+    cout << endl ;   
+
+    switch ( ch )
+    {
+      case 1 :
+        cout<<"First" << endl ;
+        break;
+      case '1': switch ( num ){
+                case 1 : cout<< "Value of num is "<<num << endl ;
+                break;
+               }
+                break ;       
+      default:
+        cout<<"It is default case" << endl;
+
+       cout << endl ;  
+    }
+    
+      return 0 ;
+}
